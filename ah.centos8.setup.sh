ac870a0ca1a0528e3372b1806b6802c3344f3b39
Andi Hafner <andihafner@andihafner.com>
2021-03-26 14:15:35 +0100

    ah. add vbox guest adds build dependencies to centos 8 setup

--- ah.centos8.setup.sh
+++ ah.centos8.setup.sh
@@ -1,7 +1,14 @@
+: '
+---------- andihafner.com 2021 ----------
+
+todo: make it idempotent, i.e, check if entrys are present already
+
+'
 set -o vi
-echo "source .ah.bashrc" >> .bashrc
-echo -e "\n# ---------- added by andihafner.com 2021 ----------\nset -o vi" >> .ah.bashrc
-source .bashrc
+echo -e "\n# ---------- added by andihafner.com 2021 ----------\nset -o vi" >> ../.ah.bashrc
+echo -e "\n# ---------- added by andihafner.com 2021 ----------\nsource .ah.bashrc" >> ../.bashrc
+source ../.bashrc
 sudo dnf install -y  https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-#sudo dnf install -y google-compute-engine google-osconfig-agent gce-disk-expand google-cloud-sdk
-sudo dnf install -y bash-completion mc vim neofetch
+sudo dnf install -y bash-completion mc vim neofetch etckeeper tig htop
+# to fullfill VirtualBox Guest Additions Build dependencies:
+sudo dnf install gcc make perl kernel-devel kernel-headers dkms
